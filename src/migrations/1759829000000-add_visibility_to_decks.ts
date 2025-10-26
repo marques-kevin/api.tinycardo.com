d06f1afbef0ada913ee42e8fc5e3a49d5a1740a7
Kevin Marques <marques.developpement@gmail.com>
2025-10-26 21:31:29 +0100

    fix: migration

--- src/migrations/1759829000000-add_visibility_to_decks.ts
+++ src/migrations/1759829000000-add_visibility_to_decks.ts
@@ -4,11 +4,6 @@ export class AddVisibilityToDecks1759829000000 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
       ALTER TABLE decks
-      DROP COLUMN public;
-    `);
-
-    await queryRunner.query(`
-      ALTER TABLE decks
       ADD COLUMN visibility VARCHAR(255) NOT NULL DEFAULT 'private' CHECK (visibility IN ('public', 'private', 'unlisted'));
     `);
   }
