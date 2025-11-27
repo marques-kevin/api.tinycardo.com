d525829fe60b739dce5a0b8135b25933cd490bac
Marques Kevin <marques.developpement@gmail.com>
2025-11-27 17:19:57 +0100

    Revert "feat: telemetry to open collector (#20)"
    This reverts commit f9c22ee0ef7588d2270d3d37ba773aefb0bda0bc.

--- src/instruments/telemetry.ts
+++ src/instruments/telemetry.ts
@@ -1,6 +1,7 @@
 import { NodeSDK } from '@opentelemetry/sdk-node';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
 import { SimpleLogRecordProcessor } from '@opentelemetry/sdk-logs';
+import { CompressionAlgorithm } from '@opentelemetry/otlp-exporter-base';
 import { OTLPLogExporter } from '@opentelemetry/exporter-logs-otlp-http';
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
 import { PeriodicExportingMetricReader } from '@opentelemetry/sdk-metrics';
@@ -12,33 +13,41 @@ import {
 import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
 import { NestInstrumentation } from '@opentelemetry/instrumentation-nestjs-core';
 
-const EXPORTER_ENDPOINT = process.env.OTEL_EXPORTER_OTLP_ENDPOINT;
+const resource = defaultResource().merge(
+  resourceFromAttributes({
+    [SemanticResourceAttributes.SERVICE_NAME]: 'tinycardo',
+    [SemanticResourceAttributes.SERVICE_NAMESPACE]: 'api',
+    [SemanticResourceAttributes.DEPLOYMENT_ENVIRONMENT]:
+      process.env.NODE_ENV || 'development',
+  }),
+);
 
-if (EXPORTER_ENDPOINT) {
-  const resource = defaultResource().merge(
-    resourceFromAttributes({
-      [SemanticResourceAttributes.SERVICE_NAME]: 'tinycardo',
-      [SemanticResourceAttributes.SERVICE_NAMESPACE]: 'api',
-      [SemanticResourceAttributes.DEPLOYMENT_ENVIRONMENT]:
-        process.env.NODE_ENV || 'development',
-    }),
-  );
+const headers = {
+  Authorization: `Bearer ${process.env.BETTER_STACK_TOKEN}`,
+};
 
+if (process.env.BETTER_STACK_TOKEN && process.env.BETTER_STACK_ENDPOINT) {
   const sdk = new NodeSDK({
     resource,
     traceExporter: new OTLPTraceExporter({
-      url: `${EXPORTER_ENDPOINT}/v1/traces`,
+      url: `${process.env.BETTER_STACK_ENDPOINT}/v1/traces`,
+      headers,
+      compression: CompressionAlgorithm.GZIP,
     }),
     logRecordProcessors: [
       new SimpleLogRecordProcessor(
         new OTLPLogExporter({
-          url: `${EXPORTER_ENDPOINT}/v1/logs`,
+          url: `${process.env.BETTER_STACK_ENDPOINT}/v1/logs`,
+          headers,
+          compression: CompressionAlgorithm.GZIP,
         }),
       ),
     ],
     metricReader: new PeriodicExportingMetricReader({
       exporter: new OTLPMetricExporter({
-        url: `${EXPORTER_ENDPOINT}/v1/metrics`,
+        url: `${process.env.BETTER_STACK_ENDPOINT}/v1/metrics`,
+        headers,
+        compression: CompressionAlgorithm.GZIP,
       }),
     }),
     instrumentations: [
